16f4bc3720907b7cef4e858836e8edbf58edac5c
Miguel Machado <mmachado53@gmail.com>
2020-01-06 22:02:54 -0700

    build beta 0.0.1

---
+++ build/precache-manifest.16e1f2ce4228c430b6580d29fb953d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f891c3e7e598513c91b055c859ddc2a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343d02bc393980f2d2ef",
+    "url": "/static/css/main.df1f87bf.chunk.css"
+  },
+  {
+    "revision": "e531b61a5c14e5287e56",
+    "url": "/static/js/2.df63d6a4.chunk.js"
+  },
+  {
+    "revision": "343d02bc393980f2d2ef",
+    "url": "/static/js/main.15464396.chunk.js"
+  },
+  {
+    "revision": "26805478d71032af704f",
+    "url": "/static/js/runtime-main.80e05a18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b06cd1d9143f0cfb8de181fd31ceb494",
+    "url": "/static/media/logo.b06cd1d9.png"
+  }
+]);
